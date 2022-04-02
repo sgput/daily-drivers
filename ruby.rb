a490e05b08211f518c20da4d00ed4e35b4cd1a00
Spencer Putnam <spencer@getflywheel.com>
2022-04-01 22:29:06 -0500

    Adds empty, blank and present ruby methods

--- ruby.rb
+++ ruby.rb
@@ -5,3 +5,17 @@ nil.nil?   # => true
 "".nil?    # => false
 " ".nil?   # => false
 true.nil?  # => false
+
+# `empty?`` can be used on some Ruby objects including Arrays, Hashes and Strings. It returns true only if the object's length is zero.
+nil.empty?   # NoMethodError: undefined method `empty?' for nil:NilClass
+{}.empty?    # => true
+[].empty?    # => true
+"".empty?    # => true
+" ".empty?   # => true
+true.empty?  # => NoMethodError: undefined method `empty?' for true:TrueClass
+
+# `blank?` is an ActiveSupport extension to Ruby Object and returns true for nil, false, empty, or a white-space string.
+[ "", " ", false, nil, [], {} ].all?(&:blank?) # => true
+
+# `present?` is also an ActiveSupport extension to Ruby Object and it is the negation of blank?
+[ "", " ", false, nil, [], {} ].any?(&:present?) # => false
